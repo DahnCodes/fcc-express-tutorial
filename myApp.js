502742d47430a851913ab8cbbe39949b23cef8e4
DahnCodes <stanleydaniel994@gmail.com>
2025-08-31 23:47:01 +0100

    served html file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
-app.get("/", function(req, res){
-res.send('Hello Express');
+// app.get("/", function(req, res){
+// res.send('Hello Express');
+// })
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/views/index.html")
 })
 console.log("Hello World");
 
