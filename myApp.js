d277d93ca78782c0a48b308b41a7efa41b6bb574
DahnCodes <stanleydaniel994@gmail.com>
2025-09-07 18:30:47 +0100

    edited my middleware

--- myApp.js
+++ myApp.js
@@ -2,9 +2,16 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 // app.get("/", function(req, res){
-// res.send('Hello Express');
-// })
-
+    // res.send('Hello Express');
+    // })
+    
+    app.use((req, res, next) =>{
+        console.log(`${req.method} ${req.path} - ${req.ip}`);
+        next();
+    })
+    
+    app.use("/public/style.css", express.static(__dirname + "/public/style.css"));
+    
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/views/index.html")
 })
@@ -16,11 +23,6 @@ app.get("/json", (req, res) => {
     }
         res.json({"message": message});
 })
-app.use((req, res, next) =>{
-console.log(`${req.method} ${req.path} ${req.ip}`);
-next();
-})
-app.use("/public/style.css", express.static(__dirname + "/public/style.css"));
 console.log("Hello World");
 
 // let method = "GET"
