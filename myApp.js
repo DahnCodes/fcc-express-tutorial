f50a66335b7a9dafe772b791e221cad2d13f6cb3
DahnCodes <stanleydaniel994@gmail.com>
2025-09-07 18:16:56 +0100

    built a middleware

--- myApp.js
+++ myApp.js
@@ -9,47 +9,23 @@ app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/views/index.html")
 })
 app.get("/json", (req, res) => {
+  
     let message = "Hello json";
     if (process.env.MESSAGE_STYLE === "uppercase"){
         message = message.toUpperCase();
     }
         res.json({"message": message});
 })
+app.use((req, res, next) =>{
+console.log(`${req.method} ${req.path} ${req.ip}`);
+next();
+})
 app.use("/public/style.css", express.static(__dirname + "/public/style.css"));
 console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// let method = "GET"
+// let path = "/json"
+// let ip = "::ffff:127.0.0.1"
 
 
  module.exports = app;
